05c4e8c957478a21d27b1914e66c26eb74db357f
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2024-07-22 10:23:25 +0200

    actualizacion

---
+++ GestorInventario/GestorInventario/Application/Services/TokenGenerator.cs
@@ -0,0 +1,361 @@
+﻿using GestorInventario.Application.DTOs;
+using GestorInventario.Domain.Models;
+using GestorInventario.Interfaces.Application;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace GestorInventario.Application.Services
+{
+    public class TokenGenerator: ITokenGenerator
+    {
+        private readonly GestorInventarioContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMemoryCache _memoryCache;
+        private readonly ILogger<TokenGenerator> _logger;
+        private readonly IDistributedCache _redis;
+        public TokenGenerator(GestorInventarioContext context, IConfiguration configuration, IHttpContextAccessor httpContextAccessor, IMemoryCache memoryCache, ILogger<TokenGenerator> logger, IDistributedCache cache)
+        {
+            _context = context;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+            _memoryCache = memoryCache;
+            _logger = logger;
+            _redis = cache;
+        }
+        public async Task<DTOLoginResponse> GenerarTokenSimetrico(Usuario credencialesUsuario)
+        {
+            var usuarioDB = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == credencialesUsuario.Id);
+            var claims = new List<Claim>()
+            {
+                 new Claim(ClaimTypes.Email, credencialesUsuario.Email),
+                 new Claim(ClaimTypes.Role, credencialesUsuario.IdRolNavigation.Nombre),
+                 new Claim(ClaimTypes.NameIdentifier, credencialesUsuario.Id.ToString())
+                
+
+            };
+
+            var clave = _configuration["ClaveJWT"];
+            var claveKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(clave));
+            var signinCredentials = new SigningCredentials(claveKey, SecurityAlgorithms.HmacSha256);
+            var securityToken = new JwtSecurityToken(
+                issuer: _configuration["JwtIssuer"],
+                audience: _configuration["JwtAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: signinCredentials);
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+            return new DTOLoginResponse()
+            {
+                Id = credencialesUsuario.Id,
+                Token = tokenString,
+                Rol = credencialesUsuario.IdRolNavigation.Nombre,
+            };
+        }
+        public async Task<DTOLoginResponse> GenerarTokenAsimetricoFijo(Usuario credencialesUsuario)
+        {
+            var usuarioDB = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == credencialesUsuario.Id);
+
+            var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Email, credencialesUsuario.Email),
+                    new Claim(ClaimTypes.Role, credencialesUsuario.IdRolNavigation.Nombre),
+                    new Claim(ClaimTypes.NameIdentifier, credencialesUsuario.Id.ToString())
+                  
+                };
+            // Carga la clave privada desde la configuración
+            var privateKey = _configuration["JWT:PrivateKey"];
+
+            // Convierte la clave privada a formato RSA
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(privateKey);
+
+            // Crea las credenciales de firma con la clave privada
+            var signinCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
+            var securityToken = new JwtSecurityToken(
+                issuer: _configuration["JwtIssuer"],
+                audience: _configuration["JwtAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: signinCredentials);
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+
+            return new DTOLoginResponse()
+            {
+                Id = credencialesUsuario.Id,
+                Token = tokenString,
+                Rol = credencialesUsuario.IdRolNavigation.Nombre,
+            };
+        }
+        // SIN USAR REDIS
+        //public async Task<DTOLoginResponse> GenerarTokenAsimetricoDinamico(Usuario credencialesUsuario)
+        //{
+        //    var usuarioDB = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == credencialesUsuario.Id);
+
+        //    var claims = new List<Claim>()
+        //    {
+        //        new Claim(ClaimTypes.Email, credencialesUsuario.Email),
+        //        new Claim(ClaimTypes.Role, credencialesUsuario.IdRolNavigation.Nombre),
+        //        new Claim(ClaimTypes.NameIdentifier, credencialesUsuario.Id.ToString())
+        //    };
+
+        //    /*Hacemos una llamada al servicio "RSACryptoServiceProvider" encargado de la gestion y configuracion de la claves rsa o clave
+        //     asimetrica, el motivo por el que se poner "RSACryptoServiceProvider(2048)" es para decir que longitud va a tener nuestra clave
+        //    rsa en este caso va a tener una longitud de 2048 bits que es la longitud estandar para claves rsa*/
+        //    using (var rsa = new RSACryptoServiceProvider(2048))
+        //    {
+        //        /*Para el cifrado que vamos a realizar primero creamos las 2 claves asimetricas de manera dinamica 
+        //         1 clave privada "privateKey" y 1 clave publica "publicKey"*/
+        //        var privateKey = rsa.ExportParameters(true);
+        //        var publicKey = rsa.ExportParameters(false);
+        //        /*Despues generamos de manera dinamica nuestra clave aes o clave simetrica, en la primera variable 
+        //         " var aes = Aes.Create()" esto crea el objeto criptográfico que almacenara la clave simetrica,
+        //        para generar dicha clave ponemos esto " aes.GenerateKey();" y por ultimo guardamos en una variable
+        //        el valor de esa clave "var aesKey = aes.Key;"*/
+        //        var aes = Aes.Create();
+        //        aes.GenerateKey();
+        //        var aesKey = aes.Key;
+        //        /*Una vez creadas ambas claves aes(simetrico) y rsa(asimetrico), lo primero que hacemos es hacer uso del valor
+        //         de la clave publica que hemos generado de esta manera "rsa.ImportParameters(publicKey);"*/
+        //        rsa.ImportParameters(publicKey);
+        //        /*Haciendo uso del valor de la clave publica rsa(asimetrica) ciframos nuestra clave aes(simetrica) haciendo uso del 
+        //         * metodo Encrypt de rsa, por lo tanto la clave aes queda cifrada con la clave publica rsa(asimetrica)*/
+        //        var encryptedAesKey = rsa.Encrypt(aesKey, false);
+
+
+        //        /*Aqui usamos nuestro metodo Cifrar que hemos creado el metodo que hemos echo el tipo de cifrado que hace es simetrico o aes,
+        //         pues aqui lo que se cifra es el Modulus de la clave publica con aes. ¿Que es Modulus en una clave publica asimetrica?
+        //        Es un numero muy grande  este numero es el que aporta seguridad a la clave publica. Al cifrarlo agregamos mas seguridad.*/
+        //        var publicKeyCifrada = Cifrar(publicKey.Modulus, aesKey);
+
+
+        //        // Guarda la clave AES cifrada y la clave pública cifrada en el caché en memoria
+        //        _memoryCache.Set(credencialesUsuario.Id.ToString() + "PrivateKey", privateKey);
+        //        _memoryCache.Set(credencialesUsuario.Id.ToString() + "EncryptedAesKey", encryptedAesKey);
+        //        _memoryCache.Set(credencialesUsuario.Id.ToString() + "PublicKey", publicKeyCifrada);
+        //        //Esto es como nuestro token es firmado
+        //        var signinCredentials = new SigningCredentials(new RsaSecurityKey(privateKey), SecurityAlgorithms.RsaSha256);
+
+        //        var securityToken = new JwtSecurityToken(
+        //            issuer: _configuration["JwtIssuer"],
+        //            audience: _configuration["JwtAudience"],
+        //            claims: claims,
+        //            expires: DateTime.Now.AddHours(24),
+        //            signingCredentials: signinCredentials);
+        //        var tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+
+        //        return new DTOLoginResponse()
+        //        {
+        //            Id = credencialesUsuario.Id,
+        //            Token = tokenString,
+        //            Rol = credencialesUsuario.IdRolNavigation.Nombre,
+        //        };
+        //    }
+        //}
+        // USANDO REDIS
+        public async Task<DTOLoginResponse> GenerarTokenAsimetricoDinamico(Usuario credencialesUsuario)
+        {
+            var usuarioDB = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == credencialesUsuario.Id);
+
+            var claims = new List<Claim>()
+            {
+            new Claim(ClaimTypes.Email, credencialesUsuario.Email),
+            new Claim(ClaimTypes.Role, credencialesUsuario.IdRolNavigation.Nombre),
+            new Claim(ClaimTypes.NameIdentifier, credencialesUsuario.Id.ToString())
+            };
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                var privateKey = rsa.ExportParameters(true);
+                var publicKey = rsa.ExportParameters(false);
+
+                var aes = Aes.Create();
+                aes.GenerateKey();
+                var aesKey = aes.Key;
+
+                rsa.ImportParameters(publicKey);
+                var encryptedAesKey = rsa.Encrypt(aesKey, false);
+
+                var publicKeyCifrada = Cifrar(publicKey.Modulus, aesKey);
+
+                // Convertir las claves a base64 para almacenar en Redis
+                var privateKeyJson = JsonConvert.SerializeObject(privateKey);
+                var encryptedAesKeyBase64 = Convert.ToBase64String(encryptedAesKey);
+                var publicKeyCifradaBase64 = Convert.ToBase64String(publicKeyCifrada);
+
+                // Guarda la clave AES cifrada y la clave pública cifrada en Redis
+                await _redis.SetStringAsync(credencialesUsuario.Id.ToString() + "PrivateKey", privateKeyJson);
+                await _redis.SetStringAsync(credencialesUsuario.Id.ToString() + "EncryptedAesKey", encryptedAesKeyBase64);
+                await _redis.SetStringAsync(credencialesUsuario.Id.ToString() + "PublicKey", publicKeyCifradaBase64);
+
+                var signinCredentials = new SigningCredentials(new RsaSecurityKey(privateKey), SecurityAlgorithms.RsaSha256);
+
+                var securityToken = new JwtSecurityToken(
+                    issuer: _configuration["JwtIssuer"],
+                    audience: _configuration["JwtAudience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddHours(24),
+                    signingCredentials: signinCredentials);
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+
+                return new DTOLoginResponse()
+                {
+                    Id = credencialesUsuario.Id,
+                    Token = tokenString,
+                    Rol = credencialesUsuario.IdRolNavigation.Nombre,
+                };
+            }
+        }
+
+
+
+        /*Nuestro metodo cifrar devuelve un array de bytes este metodo se compone de lo que se va a cifrar que es el "data" y con que se va a 
+         cifrar.*/
+        public byte[] Cifrar(byte[] data, byte[] aesKey)
+        {
+            try
+            {
+                /*En este caso usamos aes para cifrar, la manera de inicializarlo es asi "var aes = Aes.Create()" pero primero hay que poner
+                 un using.*/
+                using (var aes = Aes.Create())
+                {
+                    //Le pasamo la llave con la que va a cifrar
+                    aes.Key = aesKey;
+                    //El modo de cifrado en este caso es CBC esto es que va tomando parte a parte de los datos y los va cifrando
+                    aes.Mode = CipherMode.CBC;
+                    //Cuando llega al final y no tiene datos suficientes ha cifrar usa este metodo de relleno
+                    aes.Padding = PaddingMode.PKCS7;
+                    //Generamos un vector de inicializacion que es un valor aleatorio
+                    aes.GenerateIV();
+                    /*Esto es lo que realmente cifra los datos, aqui creamos el objeto encargado de cifrar "var encryptor = aes.CreateEncryptor()"
+                     esta variable tiene que ir dentro de un using
+                     */
+                    using (var encryptor = aes.CreateEncryptor())
+                    {
+                       
+                        /*El metodo TransformFinalBlock dentro de encryptor recibe 3 parametros el primero son los datos, despues se dice 
+                         como se inicializa el array y se obtiene la longitud de los datos y se cifra*/
+                        var cipherText = encryptor.TransformFinalBlock(data, 0, data.Length);
+                        //Concatena el vector de inicializacion con el valor cifrado y lo convierte a un array
+                        return aes.IV.Concat(cipherText).ToArray(); 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                _logger.LogCritical("Error al cifrar", ex);
+                throw;
+            }
+        }
+    
+    /*Aqui tenemos nuestro metodo descrifrar que devuelve un array de bytes y recibe 2 parametros el primero la informacion cifrada
+     y el segundo unos valores especiales de la clave privada*/
+    public byte[] Descifrar(byte[] encryptedData, RSAParameters privateKeyParams)
+        {
+            try
+            {
+                //Llamamos al servicio que ha encriptado nuestros datos
+                using (var rsa = new RSACryptoServiceProvider())
+                {
+                    //Extrae el valor de la clave privada
+                    rsa.ImportParameters(privateKeyParams);
+                    //Haciendo uso de la clave privada descifra los datos
+                    var decryptedAesKey = rsa.Decrypt(encryptedData, false);
+                    //retorna los datos descifrados
+                    return decryptedAesKey;
+                }
+            }
+            catch (Exception ex)
+            {
+                var collectioncookies = _httpContextAccessor.HttpContext?.Request.Cookies;
+                foreach (var cookie in collectioncookies!)
+                {
+                    _httpContextAccessor.HttpContext?.Response.Cookies.Delete(cookie.Key);
+                }
+                if (_httpContextAccessor.HttpContext?.Request.Path != "/Auth/Login")
+                {
+                    _httpContextAccessor.HttpContext?.Response.Redirect("/Auth/Login");
+                }
+                _logger.LogCritical("Error al descifrar", ex);
+                return new byte[0];
+            }
+        }
+/*Aqui tenemos otro metodo descifrar pero este descifra la clave asimetrica recibe los datos y la clave*/
+        public byte[] Descifrar(byte[] data, byte[] aesKey)
+        {
+            try
+            {
+                // Descifra los datos con la clave AES
+                using (var aes = Aes.Create())
+                {
+                    //Se pasa la clave aes
+                    aes.Key = aesKey;
+                    //Se dice con que metodo se cifro
+                    aes.Mode = CipherMode.CBC;
+                    //Que relleno se uso para el cifrado
+                    aes.Padding = PaddingMode.PKCS7;
+                    //De los datos cifrados primero obtine el valor de la clave lo divide entre 8 y lo convierte a un array
+                    var iv = data.Take(aes.BlockSize / 8).ToArray();
+                    //Aqui salta a los siguientes datos cifrados y hace igual que el anterior
+                    var cipherText = data.Skip(aes.BlockSize / 8).ToArray();
+                    //Se facilita el vector de inicializacion usado
+                    aes.IV = iv;
+                    //Esto es lo que realmente descifra los datos
+                    using (var decryptor = aes.CreateDecryptor())
+                    {
+                        //se pasa el valor cifrado como se inicializo y se obtiene la longitud del valor por ultimo se descifra
+                        return decryptor.TransformFinalBlock(cipherText, 0, cipherText.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var collectioncookies = _httpContextAccessor.HttpContext?.Request.Cookies;
+                foreach (var cookie in collectioncookies!)
+                {
+                    _httpContextAccessor.HttpContext?.Response.Cookies.Delete(cookie.Key);
+                }
+                if (_httpContextAccessor.HttpContext?.Request.Path != "/Auth/Login")
+                {
+                    _httpContextAccessor.HttpContext?.Response.Redirect("/Auth/Login");
+                }
+                _logger.LogCritical("Error al descifrar", ex);
+                return new byte[0];
+            }
+        }
+        public async Task<DTOLoginResponse> GenerarTokenSinCifrar(Usuario credencialesUsuario)
+        {
+            var usuarioDB = await _context.Usuarios.FirstOrDefaultAsync(x => x.Id == credencialesUsuario.Id);
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, credencialesUsuario.Email),
+                new Claim(ClaimTypes.Role, credencialesUsuario.IdRolNavigation.Nombre),
+                new Claim(ClaimTypes.NameIdentifier, credencialesUsuario.Id.ToString())
+            };
+            var clave = _configuration["ClaveJWT"];
+            var claveKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(clave));
+            var signinCredentials = new SigningCredentials(claveKey, SecurityAlgorithms.HmacSha256);
+            var securityToken = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: signinCredentials);
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+            return new DTOLoginResponse()
+            {
+                Id = credencialesUsuario.Id,
+                Token = tokenString,
+                Rol = credencialesUsuario.IdRolNavigation.Nombre,
+            };
+        }
+
+    }
+}
---
+++ GestorInventario/GestorInventario/Program.cs
@@ -0,0 +1,198 @@
+﻿using GestorInventario.Application.Services;
+using GestorInventario.Domain.Models;
+using GestorInventario.Infraestructure.Repositories;
+using GestorInventario.Infrastructure.Repositories;
+using GestorInventario.Interfaces;
+using GestorInventario.Interfaces.Application;
+using GestorInventario.Interfaces.Infraestructure;
+using GestorInventario.Interfaces.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using GestorInventario.Application.Politicas_Resilencia;
+using GestorInventario.PaginacionLogica;
+using GestorInventario.Middlewares;
+using GestorInventario.Configuracion;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.IO.Pipelines;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Sockets;
+using System.Collections;
+
+var builder = WebApplication.CreateBuilder(args);
+// Agregar variables de entorno a la configuración
+builder.Configuration.AddEnvironmentVariables();
+
+string secret = builder.Configuration["ClaveJWT"];
+//Para usar las variables de entorno ejecutar el archivo SetEnvironmentVariable.ps1
+builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); ;
+
+var isDocker = Environment.GetEnvironmentVariable("IS_DOCKER") == "true";
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+string connectionString;
+
+if (isDocker)
+{
+    connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True";
+}
+else
+{
+    connectionString = $"Data Source={dbHost};Initial Catalog={dbName};Integrated Security=True;TrustServerCertificate=True";
+}
+builder.Services.AddDbContext<GestorInventarioContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+}
+);
+builder.Services.AddMvc();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+builder.Logging.AddLog4Net();
+
+//Servicios
+builder.Services.AddTransient<GenerarPaginas>();
+builder.Services.AddTransient<PaginacionMetodo>();
+builder.Services.AddTransient<IGestorArchivos, GestorArchivosService>();
+builder.Services.AddTransient<INewStringGuid, NewStringGuid>();
+builder.Services.AddTransient<HashService>();
+builder.Services.AddTransient<ICarritoRepository, CarritoRepository>();
+builder.Services.AddTransient<IChangePassService, ChangePassService>();
+builder.Services.AddTransient<IConfirmEmailService, ConfirmEmailService>();
+builder.Services.AddTransient<IEmailService, EmailService>();
+builder.Services.AddTransient<TokenService>();
+builder.Services.AddTransient<ITokenGenerator, TokenGenerator>();
+builder.Services.AddTransient<TokenGenerator>();
+builder.Services.AddTransient<IAdminRepository,AdminRepository>();
+builder.Services.AddTransient<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddTransient<IPedidoRepository,PedidoRepository>();
+builder.Services.AddTransient<PolicyHandler>();
+builder.Services.AddTransient<IProductoRepository, ProductoRepository>();
+builder.Services.AddTransient<IProveedorRepository,ProveedorRepository>();  
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddMemoryCache();
+//builder.Services.AddStackExchangeRedisCache(options =>
+//{
+//    string redisConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
+//    if (string.IsNullOrEmpty(redisConnectionString))
+//    {
+
+//        redisConnectionString = builder.Configuration["Redis:ConnectionString"];
+//    }
+//    options.Configuration = redisConnectionString;
+//    options.InstanceName = "SampleInstance";
+//});
+
+bool useRedis = bool.Parse(Environment.GetEnvironmentVariable("USE_REDIS") ?? "false");
+
+if (useRedis)
+{
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        string redisConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
+        if (string.IsNullOrEmpty(redisConnectionString))
+        {
+            redisConnectionString = builder.Configuration["Redis:ConnectionString"];
+        }
+        options.Configuration = redisConnectionString;
+        options.InstanceName = "SampleInstance";
+    });
+}
+
+/* ¿Como cambiar el metodo de autenticación?
+ * 1º Cerrar todas las sesiones activas
+ * 2º Comentar la obción actual y descomantar la deseada
+ * 3º En la parte de middleware se efectuara lo mismo conforme a la configuración elegida
+ * 4º Vamos al archivo TokenService.cs ubicado en Application-->Services-->TokenService.cs y ahi hacemos lo mismo comentamos la opcion
+ * actual y descomentamos la que se vaya a usar
+ */
+//builder.ConfiguracionSimetrica(builder.Configuration);
+//builder.ConfiguracionAsimetricaFija(builder.Configuration);
+builder.ConfiguracionAsimetricaDinamica(builder.Configuration);
+//builder.ConfiguracionSimetricaSencilla(builder.Configuration);
+
+
+/*
+
+    * options.Preload = true;: Esta opción indica que quieres incluir tu sitio en la lista de precarga de HSTS. 
+    Los navegadores mantienen esta lista de sitios que deben ser accedidos solo a través de HTTPS.
+
+    * options.IncludeSubDomains = true;: Esta opción indica que la política de HSTS debe aplicarse a todos los subdominios 
+    del dominio actual.
+
+    * options.MaxAge = TimeSpan.FromDays(60);: Esta opción establece la cantidad de tiempo que los navegadores deben recordar 
+    que este sitio solo debe ser accedido usando HTTPS.
+
+    * options.ExcludedHosts.Add("example.com"); y options.ExcludedHosts.Add("www.example.com");: Estas opciones permiten excluir 
+    ciertos hosts de la política de HSTS. En este caso, “example.com” y “www.example.com” están excluidos.
+*/
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(60);
+    //options.ExcludedHosts.Add("example.com");
+    //options.ExcludedHosts.Add("www.example.com");
+});
+
+
+builder.Services.AddMvc();
+builder.Services.AddSession(options =>
+{
+    //Si el usuario esta inactivo 20 minutos la sesion se cierra automaticamente
+    options.IdleTimeout = TimeSpan.FromMinutes(50);
+    //evita que las operaciones lentas bloqueen el servidor
+    options.IOTimeout = TimeSpan.FromMinutes(30);
+    //La cookie solo es accesible por el servidor
+    options.Cookie.HttpOnly = true;
+    //Cuando el usuario rechaza las cookies esto no se vera afectado
+    options.Cookie.IsEssential = true;
+    //Cuando el usuario rechaza las cookies esto no se vera afectado
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors();
+app.UseRouting();
+//Identifica quien es el usuario
+app.UseAuthentication();
+//Determina que puede hacer o no el usuario
+app.UseAuthorization();
+app.UseSession();
+
+
+//app.MiddlewareAutenticacionSimetrica(builder);
+//app.MiddlewareAutenticacionAsimetricaFija(builder);
+app.MiddlewareAutenticacionAsimetricaDinamica(builder);
+//app.MiddlewareAutenticacionSimetricaSencilla(builder);
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
