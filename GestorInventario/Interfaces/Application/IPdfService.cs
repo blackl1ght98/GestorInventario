3364a1ebb50a2c00c3dd6418acd53253b48c2c37
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2025-03-14 19:14:05 +0100

    a

--- GestorInventario/Interfaces/Application/IPdfService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GestorInventario.Interfaces.Application
-{
-    public interface IPdfService
-    {
-        Task<(bool, string, byte[])> DescargarPDF();
-        Task<(bool, string, byte[])> DescargarProductoPDF();
-    }
-}
--- GestorInventario/Interfaces/IGestorArchivos.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestorInventario.Interfaces
-{
-    public interface IGestorArchivos
-    {
-        Task<string> EditarArchivo(byte[] contenido, string extension, string carpeta, string ruta);
-        Task BorrarArchivo(string ruta, string carpeta);
-        Task<string> GuardarArchivo(byte[] contenido, string extension, string carpeta);
-    }
-}
