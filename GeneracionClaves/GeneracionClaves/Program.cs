6128e7724becb377f79d33367db8806f42650178
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2025-04-25 22:28:00 +0200

    repo reparado

---
+++ GeneracionClaves/GeneracionClaves/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Cryptography;
+
+var rsa = new RSACryptoServiceProvider(2048); // 2048 es el tamaño de la clave
+Console.WriteLine("Clave privada:");
+Console.WriteLine(rsa.ToXmlString(true)); // true para incluir la clave privada en la salida
+Console.WriteLine();
+Console.WriteLine("Clave pública:");
+Console.WriteLine(rsa.ToXmlString(false));
