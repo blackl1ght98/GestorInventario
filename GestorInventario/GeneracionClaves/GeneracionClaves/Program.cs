153e08f2a6a6f4f206ebce6096244d1f316086ea
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2025-05-02 15:33:02 +0200

    actualizacion repo

--- GestorInventario/GeneracionClaves/GeneracionClaves/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Security.Cryptography;
-
-var rsa = new RSACryptoServiceProvider(2048); // 2048 es el tamaño de la clave
-Console.WriteLine("Clave privada:");
-Console.WriteLine(rsa.ToXmlString(true)); // true para incluir la clave privada en la salida
-Console.WriteLine();
-Console.WriteLine("Clave pública:");
-Console.WriteLine(rsa.ToXmlString(false));
--- GestorInventario/GestorInventario/Application/DTOs/CambiarRolRequestDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GestorInventario.Application.DTOs
-{
-    public class CambiarRolRequestDTO
-    {
-        public int Id { get; set; }
-        public int RolId { get; set; }
-    }
-}
