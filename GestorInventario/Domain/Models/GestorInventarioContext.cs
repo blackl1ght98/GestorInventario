7536f4aeb87ebc4e347436370f885fddec50d285
Guillermo Fuentes <45331658+blackl1ght98@users.noreply.github.com>
2025-06-28 20:06:27 +0200

    Actualizar contexto

--- GestorInventario/Domain/Models/GestorInventarioContext.cs
+++ GestorInventario/Domain/Models/GestorInventarioContext.cs
@@ -59,7 +59,7 @@ public partial class GestorInventarioContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-2TL9C3O\\SQLEXPRESS;Initial Catalog=GestorInventario;User ID=sa;Password=SQL#1234;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=GUILLERMO\\SQLEXPRESS;Initial Catalog=GestorInventario;User ID=sa;Password=SQL#1234;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -440,6 +440,9 @@ public partial class GestorInventarioContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PK__Permisos__3214EC0775746617");
 
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(100)
+                .IsUnicode(false);
             entity.Property(e => e.Nombre)
                 .HasMaxLength(100)
                 .IsUnicode(false);
