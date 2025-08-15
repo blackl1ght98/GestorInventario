47ab09aff09fb17610afdbf05a73d8eb6c6c1272
Guillermo Fuentes <45331658+blackl1ght98@users.noreply.github.com>
2025-08-15 17:00:44 +0200

    Refactorizar proyecto

--- GestorInventario/Domain/Models/GestorInventarioContext.cs
+++ GestorInventario/Domain/Models/GestorInventarioContext.cs
@@ -54,8 +54,25 @@ public partial class GestorInventarioContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=GUILLERMO\\SQLEXPRESS;Initial Catalog=GestorInventario;User ID=sa;Password=SQL#1234;TrustServerCertificate=True");
+    {
+        var isDocker = Environment.GetEnvironmentVariable("IS_DOCKER") == "true";
+
+        if (isDocker)
+        {
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+
+            var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        else
+        {
+            // Cadena de conexión en duro para entorno local
+            var connectionString = "Data Source=GUILLERMO\\SQLEXPRESS;Initial Catalog=GestorInventario;User ID=sa;Password=SQL#1234;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
