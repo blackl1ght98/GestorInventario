a8ade3a15d7c9e8884a5972f02dc283be44c61ef
Guillermo Fuentes <45331658+blackl1ght98@users.noreply.github.com>
2025-08-27 10:05:15 +0200

    Ver rembolsos pendientes y eliminar rembolsos

---
+++ GestorInventario/Interfaces/Infraestructure/IRembolsoRepository.cs
@@ -0,0 +1,11 @@
+﻿using GestorInventario.Application.DTOs;
+using GestorInventario.Domain.Models;
+
+namespace GestorInventario.Interfaces.Infraestructure
+{
+    public interface IRembolsoRepository
+    {
+        Task<IQueryable<Rembolso>> ObtenerRembolsos();
+        Task<(bool, string)> EliminarRembolso(int id);
+    }
+}
---
+++ GestorInventario/ViewModels/Rembolso/RembolsosViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GestorInventario.Domain.Models; 
+using GestorInventario.PaginacionLogica;
+using System.Collections.Generic;
+
+namespace GestorInventario.ViewModels
+{
+    public class RembolsosViewModel
+    {
+        public List<Rembolso> Rembolsos { get; set; }  
+        public List<PaginasModel> Paginas { get; set; }  
+        public int TotalPaginas { get; set; }
+        public int PaginaActual { get; set; }
+        public string Buscar { get; set; }
+    }
+}
