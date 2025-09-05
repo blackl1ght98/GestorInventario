f6a69d6d234cdc4a79f47663ac28c7d9ef2a3383
Guillermo Fuentes <45331658+blackl1ght98@users.noreply.github.com>
2025-09-05 08:34:08 +0200

    Correjir errores

---
+++ GestorInventario/Application/DTOs/Response paypal/POST/CreateProductRequest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace GestorInventario.Application.DTOs.Response_paypal.POST
+{
+    public class CreateProductRequest
+    {
+        [JsonProperty("name")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("image_url")]
+        public string Imagen { get; set; }
+    }
+}
