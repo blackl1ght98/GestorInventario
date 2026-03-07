7dcc9c4409a9d6db84567236c4642c37c4bedf50
Guillermo Fuentes <45331658+blackl1ght98@users.noreply.github.com>
2026-03-07 14:39:49 +0100

    correccion de errores

--- GestorInventario/Application/DTOs/Response paypal/POST/SuspendSubscriptionRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GestorInventario.Application.DTOs.Response_paypal.POST
-{
-    public class SuspendSubscriptionRequestDto
-    {
-        public required string Id { get; set; }
-        public required string Reason { get; set; }
-    }
-}
