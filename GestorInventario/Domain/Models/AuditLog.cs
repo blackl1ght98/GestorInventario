c7ee488c697c249c93576592c70e5f471cce3d3b
Guillermo Fuentes <45331658+blackl1ght98@users.noreply.github.com>
2026-04-10 15:31:08 +0200

    tabla AuditLog agregada

---
+++ GestorInventario/Domain/Models/AuditLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestorInventario.Domain.Models;
+
+public partial class AuditLog
+{
+    public long Id { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public int? UsuarioId { get; set; }
+
+    public string Tabla { get; set; } = null!;
+
+    public string Operacion { get; set; } = null!;
+
+    public int RegistroId { get; set; }
+
+    public string? Campo { get; set; }
+
+    public string? ValorAnterior { get; set; }
+
+    public string? ValorNuevo { get; set; }
+
+    public string? IpAddress { get; set; }
+
+    public string? Descripcion { get; set; }
+}
